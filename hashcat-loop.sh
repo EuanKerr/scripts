2a16ee4e77d580ee9ef90027a18713b1a8a31740
Euan <EuanKerr@users.noreply.github.com>
2024-05-06 15:04:20 -0500

    Update hashcat-loop.sh

--- hashcat-loop.sh
+++ hashcat-loop.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-shopt -s expand_aliases
-
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 </path/to/hashes.txt> <numeric-hash-type>"
     exit 1
@@ -11,86 +9,56 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-hashfile="${1}"
-type="${2}"
-hashcat_bin="/opt/tools/hashcat/hashcat.bin"
-hashcat_pot="/data/potfiles/hashcat.potfile"
-temp_potfile="$(mktemp)"
-output_file="$(basename ${hashfile})"
+HASHFILE="${1}"
+TYPE="${2}"
+HASHCAT_BIN="/opt/tools/hashcat/hashcat.bin"
+WORDLIST="/data/wordlists/combined.LARGE.txt"
+PREVIOUSLY_CRACKED_WORDLIST="/data/wordlists/previously_cracked.txt"
+RULES="/opt/tools/hashcat/rules"
+TEMP_POTFILE="$(mktemp)"
+OUTPUT_FILE="/data/output/$(date -I)_$(basename ${HASHFILE}).cracked.txt"
 
-trap '{ echo -e "\nCatching exit, copying temp potfiles to... ${hashcat_pot}"; \
-        cat "$temp_potfile" >> "$hashcat_pot"; \
-        cat ${temp_potfile} | tee -a /data/output/$(date -I)-${output_file}-cracked.txt; \
-        rm -f -- "$temp_potfile"; \
-        }' EXIT
+clean_up() {
+    cat "${TEMP_POTFILE}" >> "${OUTPUT_FILE}"
+    rm -f -- "${TEMP_POTFILE}"
+}
 
-wordlist="/data/wordlists/combined.LARGE.txt"
+trap clean_up EXIT
 
-echo "checking main potfile first for hashes - this will speed up rest of runs, this will take a few mins..."
-${hashcat_bin} --potfile-path ${hashcat_pot} -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" -o ${temp_potfile} --show
+# Check if files and directories exist
+for file in "${HASHCAT_BIN}" "${WORDLIST}" "${PREVIOUSLY_CRACKED_WORDLIST}"; do
+    if [ ! -f "$file" ]; then
+        echo "Error: File $file does not exist"
+        exit 1
+    fi
+done
 
-# use the temp potfile to speed up initial searching - we already know the rest don't exist in our main potfile
-alias hashcat="${hashcat_bin} --potfile-path ${temp_potfile}"
+function run_hashcat() {
+    "${HASHCAT_BIN}" --potfile-path "${TEMP_POTFILE}" -m "${TYPE}" -w 4 -O "${@}"
 
-hashcat -m "${type}" -a 3 -w 4 -O "${hashfile}" -i -1 '?l?d?u!"£$%^&*()_+@#' ?1?1?1?1?1?1?1?1
+    HASHCAT_STATUS=$?
+    if [ $HASHCAT_STATUS -lt 0 ]; then
+        echo "Hashcat failed with error code $HASHCAT_STATUS"
+        exit 1
+    fi
+}
 
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/OneRuleToRuleThemAll.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/passphrase-rule1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/passphrase-rule2.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/Incisive-leetspeak.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/InsidePro-HashManager.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/InsidePro-PasswordsPro.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlC-insert_00-99_1950-2050_toprules_0_F.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlC-insert_space_and_special_0_F.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlC-insert_top_100_passwords_1_G.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlC.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlCv1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/T0XlCv2.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/best64.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/combinator.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/d3ad0ne.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/d3adhob0.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/dive.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/generated.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/generated2.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/generated3.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/hob064.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/leetspeak.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/oscommerce.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/rockyou-30000.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/specific.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/toggles1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/toggles2.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/toggles3.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/toggles4.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/toggles5.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/unix-ninja-leetspeak.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hashorg.v6.hybrid.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hashorg.v6.one.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hashorg.v6.popular.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hashorg.v6.random.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hashorg.v6.raw1m.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.hybrid.royce.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.one.royce.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.popular.royce.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/hashcat/rules/pantagrule.random.royce.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-o1i1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-i1o1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-o1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nsa64.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/_NSAKEY.v2.dive.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-o2.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-append4.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/T0XlC_3_rule.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/T0XlC_insert_HTML_entities_0_Z.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-i1.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-repeater-i.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/_NSAKEY.v1.dive.rule
-hashcat -m "${type}" -a 0 -w 4 -O "${hashfile}" "${wordlist}" --loopback -r /opt/tools/hashcat-rules/nyxgeek-i2.rule
+# Check against previously cracked hashes
+run_hashcat -a 0 "${HASHFILE}" "${PREVIOUSLY_CRACKED_WORDLIST}"
 
-hashcat -m "${type}" -a 3 -w 4 -O "${hashfile}" -1 '?l?d?u!"£$%^&*()_+@#' ?1?1?1?1?1?1?1?1
-hashcat -m "${type}" -a 3 -w 4 -O "${hashfile}" -i ?a?a?a?a?a?a?a?a?a?a?a
+# Check up to 7 characters, incremetally - upper, lower, digit, some special characters
+run_hashcat -a 3 -i -1 '?l?d?u!"£$%^&*()_+@#' "${HASHFILE}" ?1?1?1?1?1?1?1
 
-cat ${temp_potfile} >>${hashcat_pot}
-cat ${temp_potfile} | tee ~/$(date -I)-${output_file}-cracked.txt
-rm -f ${temp_potfile}
+# Check with big wordlist
+run_hashcat -a 0 "${HASHFILE}" "${WORDLIST}"
+
+# Check with big wordlist and rules
+run_hashcat -a 0 --loopback -r "${RULES}/OneRuleToRuleThemStill.rule" "${HASHFILE}" "${WORDLIST}"
+
+# Check 8 characters - upper, lower, digit, some special characters
+run_hashcat -a 3 -1 '?l?d?u!"£$%^&*()_+@#' "${HASHFILE}" ?1?1?1?1?1?1?1?1
+
+# Check up to 12 characters - ALL - last ditch effort
+# run_hashcat -a 3 "${HASHFILE}" ?a?a?a?a?a?a?a?a?a?a?a?a
+
+clean_up
